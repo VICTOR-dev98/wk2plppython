e513496108644530524ccb3c11d3c7e52246259a
Dev.Wanjohi <vicvickiewanjohi98@gmail.com>
2025-03-30 15:10:50 +0300

    week-2 python assignment

---
+++ answer.py
@@ -0,0 +1,25 @@
+# Question 1 empty list
+
+my_list = []
+
+#Question 2 append
+my_list.append(10)
+my_list.append(20)
+my_list.append(30)
+my_list.append(40)
+
+#Question 3 inserting to list
+my_list.insert(1, 15)
+
+#Question 4 extend list
+my_list.extend([50, 60, 70])
+
+#Question 5 remove element
+my_list.pop()
+
+#Question 6 sort list
+my_list.sort()
+
+#Question 7 find and print index
+index_of_30 = my_list.index(30)
+print("Index of 30:", index_of_30)
